f713a055d5073abf76787ffb995d14a0e72db2ba
Abdinasir Farah <Abdinasir_farah@outlook.com>
2022-09-24 00:36:13 -0600

    remove pointer events from btns container

--- main.js
+++ main.js
@@ -1,41 +1,47 @@
 const compImg = document.querySelector(".comp-img");
 const playerImg = document.querySelector(".player-img");
+const rockBtn = document.querySelector(".rock-btn");
+const paperBtn = document.querySelector(".paper-btn");
+const scissorsBtn = document.querySelector(".scissors-btn");
 const btns = document.querySelectorAll(".btn-container button");
+
 
 function getcomputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber === 3) {
     compImg.src = "/images/scissors.png";
+    return "scissors";
   } else if (randomNumber === 2) {
     compImg.src = "/images/paper.png";
+    return "paper";
   } else {
     compImg.src = "/images/rock.png";
+    return "rock";
   }
 }
 
-function getplayerChoice() {
-  const choice = btns.forEach(function (btn) {
-    btn.addEventListener("click", function () {
-      if (btn.innerText === "J") {
-        playerImg.src = "/images/rock.png";
-      } else if (btn.innerText === "K") {
-        playerImg.src = "/images/paper.png";
-      } else if (btn.innerText === "L") {
-        playerImg.src = "/images/scissors.png";
-      }
-    });
-  });
-  return choice;
+function getplayerChoice(){
+  let choice = btns.forEach(btn=>{btn.addEventListener('click',()=>btn.textContent)});
+  if (choice === 'scissors') {
+    playerImg.src = "/images/scissors.png";
+    return "scissors";
+  } else if (choice === 'paper') {
+    playerImg.src = "/images/paper.png";
+    return "paper";
+  } else {
+    playerImg.src = "/images/rock.png";
+    return "rock";
+  }
 }
 
-function playRound(playerSelection,computerSelection){
+// function playRound(playerSelection,computerSelection){
 
-  
+// }
 
-}
+// const playerSelection=getplayerChoice();
+// const computerSelection=getcomputerChoice();
 
 
+let choice = btns.forEach(btns=>{btns.addEventListener('click',()=>btns.textContent)});
 
 
-const playerSelection = getplayerChoice();
-const computerSelection = getcomputerChoice();
