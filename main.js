07e1838c80b2b1cdf05b9ce99d9c0afcfed932b9
Abdinasir Farah <Abdinasir_farah@outlook.com>
2022-09-25 01:23:39 -0600

    create a play round function

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const compImg = document.querySelector(".comp-img");
 const playerImg = document.querySelector(".player-img");
-const rockBtn = document.querySelector(".rock-btn");
-const paperBtn = document.querySelector(".paper-btn");
-const scissorsBtn = document.querySelector(".scissors-btn");
-const btns = document.querySelectorAll(".btn-container button");
+const btns = document.querySelectorAll("button h4");
+let computerScore = document.querySelector(".computer-score span");
+let playerScore = document.querySelector(".player-score span");
 
 
 function getcomputerChoice() {
@@ -20,28 +19,73 @@ function getcomputerChoice() {
   }
 }
 
-function getplayerChoice(){
-  let choice = btns.forEach(btn=>{btn.addEventListener('click',()=>btn.textContent)});
-  if (choice === 'scissors') {
-    playerImg.src = "/images/scissors.png";
-    return "scissors";
-  } else if (choice === 'paper') {
-    playerImg.src = "/images/paper.png";
-    return "paper";
-  } else {
-    playerImg.src = "/images/rock.png";
-    return "rock";
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "rock" && computerSelection === "rock") ||
+    (playerSelection === " paper" && computerSelection === "paper") ||
+    (playerSelection === "  scissors" && computerSelection === "scissors")
+  ) {
+    console.log("draw");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore.textContent++;
+    console.log('"You Lose! Paper beats Rock"');
+  } else if (playerSelection === " paper" && computerSelection === "rock") {
+    playerScore.textContent++;
+    console.log('"You Win! Paper beats Rock"');
+  } else if (playerSelection === "  scissors" && computerSelection === "rock") {
+    computerScore.textContent++;
+
+    console.log('"You Lose! Rock beats Scissors"');
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore.textContent++;
+
+    console.log('"You Win! Rock beats Scissors"');
+  } else if (
+    playerSelection === "  scissors" &&
+    computerSelection === "paper"
+  ) {
+    playerScore.textContent++;
+
+    console.log('"You Win! Scissors beats Paper"');
+  } else if (playerSelection === " paper" && computerSelection === "scissors") {
+    computerScore.textContent++;
+
+    console.log('"You Lose! Scissors beats Paper"');
+  }
+}
+function checkWinner() {
+  if (playerScore.textContent === "5") {
+    console.log(`You:${playerScore} Comp:${computerScore}`);
+    console.log("You win the game");
+  } else if (computerScore.textContent === "5") {
+    console.log(`You:${playerScore} Comp:${computerScore}`);
+    console.log("You Lost the game");
+  } else if (
+    playerScore.textContent === "5" &&
+    computerScore.textContent === "5"
+  ) {
+    console.log(`You:${playerScore} Comp:${computerScore}`);
+    console.log("Its a tie game");
   }
 }
 
-// function playRound(playerSelection,computerSelection){
+function game() {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const playerSelection = btn.innerText;
+      console.log(playerSelection);
+      const computerSelection = getcomputerChoice();
+      if (playerSelection === "  scissors") {
+        playerImg.src = "/images/scissors.png";
+      } else if (playerSelection === " paper") {
+        playerImg.src = "/images/paper.png";
+      } else {
+        playerImg.src = "/images/rock.png";
+      }
+      playRound(playerSelection, computerSelection);
+      checkWinner();
+    });
+  });
+}
 
-// }
-
-// const playerSelection=getplayerChoice();
-// const computerSelection=getcomputerChoice();
-
-
-let choice = btns.forEach(btns=>{btns.addEventListener('click',()=>btns.textContent)});
-
-
+game();
