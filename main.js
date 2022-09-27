b09674d4c70fc4b62998bb0ea600438a6f9ed6bc
Abdinasir Farah <Abdinasir_farah@outlook.com>
2022-09-26 20:14:59 -0600

    create a function that declares a winner

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const compImg = document.querySelector(".comp-img");
 const playerImg = document.querySelector(".player-img");
-const btns = document.querySelectorAll("button h4");
+const btnsContainer = document.querySelector(".btn-container");
+const btns = btnsContainer.querySelectorAll("button");
 let computerScore = document.querySelector(".computer-score span");
 let playerScore = document.querySelector(".player-score span");
-
+const info = document.querySelector(".status");
 
 function getcomputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -22,68 +23,70 @@ function getcomputerChoice() {
 function playRound(playerSelection, computerSelection) {
   if (
     (playerSelection === "rock" && computerSelection === "rock") ||
-    (playerSelection === " paper" && computerSelection === "paper") ||
-    (playerSelection === "  scissors" && computerSelection === "scissors")
+    (playerSelection === "paper" && computerSelection === "paper") ||
+    (playerSelection === "scissors" && computerSelection === "scissors")
   ) {
-    console.log("draw");
+    info.textContent = "draw";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore.textContent++;
-    console.log('"You Lose! Paper beats Rock"');
-  } else if (playerSelection === " paper" && computerSelection === "rock") {
+    info.textContent = "You Lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore.textContent++;
-    console.log('"You Win! Paper beats Rock"');
-  } else if (playerSelection === "  scissors" && computerSelection === "rock") {
+    info.textContent = "You Win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore.textContent++;
 
-    console.log('"You Lose! Rock beats Scissors"');
+    info.textContent = "You Lose! Rock beats Scissors";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore.textContent++;
 
-    console.log('"You Win! Rock beats Scissors"');
-  } else if (
-    playerSelection === "  scissors" &&
-    computerSelection === "paper"
-  ) {
+    info.textContent = "You Win! Rock beats Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore.textContent++;
 
-    console.log('"You Win! Scissors beats Paper"');
-  } else if (playerSelection === " paper" && computerSelection === "scissors") {
+    info.textContent = "You Win! Scissors beats Paper";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore.textContent++;
 
-    console.log('"You Lose! Scissors beats Paper"');
+    info.textContent = "You Lose! Scissors beats Paper";
   }
 }
 function checkWinner() {
   if (playerScore.textContent === "5") {
-    console.log(`You:${playerScore} Comp:${computerScore}`);
-    console.log("You win the game");
+    info.textContent = "You win the game";
   } else if (computerScore.textContent === "5") {
-    console.log(`You:${playerScore} Comp:${computerScore}`);
-    console.log("You Lost the game");
+    info.textContent = "You Lost the game";
   } else if (
     playerScore.textContent === "5" &&
     computerScore.textContent === "5"
   ) {
-    console.log(`You:${playerScore} Comp:${computerScore}`);
-    console.log("Its a tie game");
+    info.textContent = "Its a tie game";
   }
 }
 
 function game() {
   btns.forEach((btn) => {
     btn.addEventListener("click", function () {
-      const playerSelection = btn.innerText;
-      console.log(playerSelection);
-      const computerSelection = getcomputerChoice();
-      if (playerSelection === "  scissors") {
-        playerImg.src = "/images/scissors.png";
-      } else if (playerSelection === " paper") {
-        playerImg.src = "/images/paper.png";
-      } else {
+      const interID = setInterval(() => {
         playerImg.src = "/images/rock.png";
-      }
-      playRound(playerSelection, computerSelection);
-      checkWinner();
+        compImg.src = "/images/rock.png";
+        compImg.classList.toggle("animate");
+        playerImg.classList.toggle("animate");
+      }, 150);
+      setTimeout(() => {
+        clearInterval(interID);
+        const playerSelection = btn.querySelector("span").textContent;
+        const computerSelection = getcomputerChoice();
+        if (playerSelection === "scissors") {
+          playerImg.src = "/images/scissors.png";
+        } else if (playerSelection === "paper") {
+          playerImg.src = "/images/paper.png";
+        } else {
+          playerImg.src = "/images/rock.png";
+        }
+        playRound(playerSelection, computerSelection);
+        checkWinner();
+      }, 1500);
     });
   });
 }
